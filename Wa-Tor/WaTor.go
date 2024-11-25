6628ee252f1fb67cb7344c1015b94bc1808a1587
DoyleAaron <aarondoyler101@gmail.com>
2024-11-25 19:25:06 +0000

    Commit issue, updated

--- Wa-Tor/WaTor.go
+++ Wa-Tor/WaTor.go
@@ -51,4 +51,43 @@ func main() {
 	// Random seed for entity placement
 	rand.Int()
 
-	// ChatGPT helped m
+	// ChatGPT helped me with the grid part of the code as I was confused as to how to initially set the fish and shark positions
+	// It works by creating a grid with the dimensions of xdim and ydim and the number of sharks and fish, and then shuffling the grid to simulate randomly placing the fish and sharks around the screen
+	grid := make([]int, xdim*ydim)
+	for i := 0; i < NumShark; i++ {
+		grid[i] = 1
+	}
+	for i := NumShark; i < NumShark+NumFish; i++ {
+		grid[i] = 2
+	}
+
+	rand.Shuffle(len(grid), func(i, j int) { grid[i], grid[j] = grid[j], grid[i] })
+
+	// Initialize the window
+	rl.InitWindow(int32(windowXSize), int32(windowYSize), "Raylib Wa-Tor world")
+	defer rl.CloseWindow() // Close window when done
+
+	// Logic loop
+	// This loop will handle the running of the simulation and will run until the window is closed
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		// These for loops will iterate through the grid and draw the fish, sharks, and water to the screen
+		for i := 0; i < xdim; i++ {
+			for k := 0; k < ydim; k++ {
+				x := i * cellXSize
+				y := k * cellYSize
+				cellValue := grid[k*xdim+i]
+				if cellValue == 1 {
+					DrawShark(x, y, cellXSize, cellYSize, sharkColour)
+				} else if cellValue == 2 {
+					DrawFish(x, y, cellXSize, cellYSize, fishColour)
+				} else {
+					DrawWater(x, y, cellXSize, cellYSize, waterColour)
+				}
+			}
+		}
+
+		rl.EndDrawing()
+	}
+}
