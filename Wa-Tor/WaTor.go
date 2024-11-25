1598f402aff060bc291b97f188ad1bca3c98f3c5
DoyleAaron <aarondoyler101@gmail.com>
2024-11-25 19:24:11 +0000

    Removed Doxygen, got static fish, sharks and water working
    In this commit I removed doxygen from my project due to it being incompatible with Go code, I am now using the alternative go doc which allows you to call comments through terminal. I had to change the layout of the comments according to their documentation but it is working as expected now

--- Wa-Tor/WaTor.go
+++ Wa-Tor/WaTor.go
@@ -1,70 +1,54 @@
-// // Wa-Tor.go code
-// // See readme for license
-// //--------------------------------------------
-// // Author: Joseph Kehoe
-// // Created on 18/11/2024
-// // Modified by: Aaron Doyle
-// // Description:
-// // Creating a working version of the Wa-Tor simulation
-// // Issues:
-// // Figure out how to create a working version of the Wa-Tor simulation
-// //--------------------------------------------
-
+// Package main implements a Wa-Tor simulation using Raylib in the Go programming language
 package main
 
 import (
+	"math/rand"
+
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+// DrawFish function
+// Parameters: x, y, width, height int, fishColour rl.Color
+// Returns: None
+// Description: Draws a fish at the given x and y coordinates with the given width and height and color
+func DrawFish(x, y, width, height int, fishColour rl.Color) {
+	rl.DrawRectangle(int32(x), int32(y), int32(width), int32(height), fishColour)
+}
+
+// DrawShark function
+// Parameters: x, y, width, height int, sharkColour rl.Color
+// Returns: None
+// Description: Draws a shark at the given x and y coordinates with the given width and height and color
+func DrawShark(x, y, width, height int, sharkColour rl.Color) {
+	rl.DrawRectangle(int32(x), int32(y), int32(width), int32(height), sharkColour)
+}
+
+// DrawWater function
+// Parameters: x, y, width, height int, waterColour rl.Color
+// Returns: None
+// Description: Draws water at the given x and y coordinates with the given width and height and color
+func DrawWater(x, y, width, height int, waterColour rl.Color) {
+	rl.DrawRectangle(int32(x), int32(y), int32(width), int32(height), waterColour)
+}
+
+// Main Class
 func main() {
-	// Initialize variables
+	// Constants
 	xdim := 100
 	ydim := 100
 	windowXSize := 800
 	windowYSize := 600
 	cellXSize := windowXSize / xdim
 	cellYSize := windowYSize / ydim
-	// NumShark := 100
-	// NumFish := 100
-	// FishBreed := 3
-	// SharkBreed := 3
-	// Starve := 3
+	NumShark := 20
+	NumFish := 100
 
-	// Initialize the window
-	rl.InitWindow(int32(windowXSize), int32(windowYSize), "Raylib Wa-Tor world")
-	defer rl.CloseWindow() // Close window when done
+	// Colours of the fish, shark, and water
+	fishColour := rl.Green
+	sharkColour := rl.Red
+	waterColour := rl.Blue
 
-	// Loop to draw the rectangles that keeps going until the escape key is pressed
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.Black)
+	// Random seed for entity placement
+	rand.Int()
 
-		// Draw rectangles (each representing a fish, shark, or empty space)
-		for i := 0; i < xdim; i++ {
-			for k := 0; k < ydim; k++ {
-				fishX := float32(i * cellXSize)  // X position of the fish
-				fishY := float32(k * cellYSize)  // Y position of the fish
-				sharkX := float32(i * cellXSize) // X position of the shark
-				sharkY := float32(k * cellYSize) // Y position of the shark
-				Width := float32(80)             // Width of the rectangle
-				Height := float32(60)            // Height of the rectangle
-				fishColour := rl.Green
-				sharkColour := rl.Blue
-
-				if(){
-					
-				}
-		}
-
-		// End drawing and display the window
-		rl.EndDrawing()
-	}
-}
-
-func drawFish(fishX, fishY, Width, Height float32, fishColour rl.Color) {
-	rl.DrawRectangle(int32(fishX), int32(fishY), int32(Width), int32(Height), fishColour)
-}
-
-func drawShark(sharkX, sharkY, Width, Height float32, sharkColour rl.Color) {
-	rl.DrawRectangle(int32(sharkX), int32(sharkY), int32(Width), int32(Height), sharkColour)
-}
+	// ChatGPT helped m
