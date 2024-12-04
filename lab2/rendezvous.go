d0f81fef30a395c882d8b30b3b514271f7a4b647
DoyleAaron <aarondoyler101@gmail.com>
2024-12-04 17:52:48 +0000

    Rendezvous working

--- lab2/rendezvous.go
+++ lab2/rendezvous.go
@@ -2,40 +2,41 @@ package main
 
 import (
 	"fmt"
-	"math/rand/v2"
+	"math/rand"
 	"sync"
 	"time"
 )
 
-//Global variables shared between functions --A BAD IDEA
+func WorkWithRendezvous(wg *sync.WaitGroup, Num int, count *int, barrier chan bool, mutex *sync.Mutex) {
+	defer wg.Done() // ChatGpt gave me this idea, it works as it ensures that the goroutine is removed from the WaitGroup when it finishes
 
-func WorkWithRendezvous(wg *sync.WaitGroup, Num int, barrier chan bool) bool {
-	var X time.Duration
-	X = time.Duration(rand.IntN(5))
-	time.Sleep(X * time.Second) //wait random time amount
+	time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
 	fmt.Println("Part A", Num)
-	//Rendezvous here
 
-	barrier <- true
-
-	for i := 0; i < cap(barrier)-1; i++ {
-		<-barrier
+	mutex.Lock()     // Make sure that the go routines will wait at the barrier
+	*count++         // Increment the counter to keep track of the number of goroutines that have reached the barrier
+	if *count == 5 { // If all goroutines have reached the barrier
+		close(barrier) // Closes the barrier channel to signal the goroutines to proceed, idea provided by chatGpt
 	}
+	mutex.Unlock() // Unlock the mutex to allow the goroutines to proceed
 
-	fmt.Println("PartB", Num)
-	wg.Done()
-	return true
+	// Wait for the signal to proceed
+	<-barrier
+
+	fmt.Println("Part B", Num)
 }
 
 func main() {
-	var wg sync.WaitGroup
 	threadCount := 5
-	barrier := make(chan bool, threadCount)
+	var wg sync.WaitGroup
+	count := 0
+	mutex := &sync.Mutex{}     // Mutex to protect shared counter
+	barrier := make(chan bool) // Channel to act as the barrier for the go routines
 
 	wg.Add(threadCount)
-	for N := range threadCount {
-		go WorkWithRendezvous(&wg, N, barrier)
+	for N := 0; N < threadCount; N++ {
+		go WorkWithRendezvous(&wg, N, &count, barrier, mutex)
 	}
-	wg.Wait() //wait here until everyone (10 go routines) is done
 
+	wg.Wait() // Wait for all goroutines to finish
 }
